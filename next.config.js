4b8e4793c09b678dad610a69c6af4d6b361ac40f
victor.longon <vhlongon@gmail.com>
2022-01-17 00:03:43 +0100

    update and simplify next config

--- next.config.js
+++ next.config.js
@@ -1,35 +1,16 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-};
-
-module.exports = (nextConfig = nextConfig) => {
-  return Object.assign({}, nextConfig, {
-    webpack(config, options) {
-      if (!options.defaultLoaders) {
-        throw new Error(
-          'This plugin is not compatible with Next.js versions below 5.0.0 https://err.sh/next-plugins/upgrade'
-        );
-      }
-
-      const { dir } = options;
-
-      config.module.rules.push({
-        test: /\.(graphql|gql)$/,
-        include: [dir],
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'graphql-tag/loader',
-          },
-        ],
-      });
-
-      if (typeof nextConfig.webpack === 'function') {
-        return nextConfig.webpack(config, options);
-      }
-
-      return config;
-    },
-  });
+module.exports = {
+  images: {
+    domains: ['avatars.githubusercontent.com'],
+  },
+  webpack: config => {
+    config.module.rules.push({
+      test: /\.(graphql|gql)$/,
+      exclude: /node_modules/,
+      loader: 'graphql-tag/loader',
+    });
+    return config;
+  },
+  webpackDevMiddleware: config => {
+    return config;
+  },
 };
