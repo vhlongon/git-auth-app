ddc50350a523774eaafdb8e65f2e05b2f162a794
victor.longon <vhlongon@gmail.com>
2022-01-25 22:51:47 +0100

    update client to work client side

--- apollo/client.ts
+++ apollo/client.ts
@@ -1,6 +1,32 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+const authLink = setContext((_, { headers, ...rest }) => {
+  console.log('_', _);
+  console.log({ headers, rest });
+  // get the authentication token from local storage if it exists
+  // const token = localStorage.getItem('token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      // authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const createHttpLink = () => {
+  return new HttpLink({
+    uri:
+      typeof window === 'undefined'
+        ? `${process.env.BASE_URL}/api/graphql`
+        : '/api/graphql',
+    credentials: 'same-origin',
+  });
+};
 
 export const client = new ApolloClient({
-  uri: `${process.env.BASE_URL}/api/graphql`,
   cache: new InMemoryCache(),
+  ssrMode: typeof window === 'undefined',
+  link: authLink.concat(createHttpLink()),
 });
