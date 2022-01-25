8d7daa9d07758d38ff6e0d5e7371275df1c437f4
victor.longon <vhlongon@gmail.com>
2022-01-25 23:17:06 +0100

    clean client

--- apollo/client.ts
+++ apollo/client.ts
@@ -2,8 +2,6 @@ import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
 const authLink = setContext((_, { headers, ...rest }) => {
-  console.log('_', _);
-  console.log({ headers, rest });
   // get the authentication token from local storage if it exists
   // const token = localStorage.getItem('token');
   // return the headers to the context so httpLink can read them
