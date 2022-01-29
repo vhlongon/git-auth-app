9882e62cfcd975e0344af4385f312b92febdde32
victor.longon <vhlongon@gmail.com>
2022-01-29 22:56:05 +0100

    dont send type name as prop in queries and mutations

--- apollo/client.ts
+++ apollo/client.ts
@@ -25,6 +25,7 @@ const createHttpLink = () => {
 
 export const client = new ApolloClient({
   cache: new InMemoryCache({
+    addTypename: false,
     typePolicies: {
       Query: {
         fields: {
