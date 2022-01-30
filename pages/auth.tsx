33b245a21629b2f1ba1d9af426bf5692d4265f92
victor.longon <vhlongon@gmail.com>
2022-01-30 02:55:58 +0100

    re-add params to set server cookie

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -73,6 +73,8 @@ export const getServerSideProps: GetServerSideProps = async context => {
           httpOnly: true,
           secure: process.env.NODE_ENV !== 'development',
           maxAge: 60 * 60,
+          sameSite: 'strict',
+          path: '/',
         },
       );
 
